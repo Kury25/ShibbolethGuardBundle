56d40c4ed9ee6ba9ced2e0331fbc0af3396f7cb9
Kury25 <24892792+Kury25@users.noreply.github.com>
2019-03-10 18:08:31 +0000

    dependancies

--- Security/ShibbolethAuthenticator.php
+++ Security/ShibbolethAuthenticator.php
@@ -3,14 +3,13 @@
 namespace GaussAllianz\ShibbolethGuardBundle\Security;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Psr\Log\LoggerInterface;
 use GaussAllianz\ShibbolethGuardBundle\Security\UsernameAttributeNotGivenException;
 use Exception;
